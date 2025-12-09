a4743b3944294dbaeb757709fec432c892362f6b
Molitor István <istvan.molitor@centralmediacsoport.hu>
2025-12-09 20:32:11 +0100

    ProductPicker

---
+++ src/Forms/Components/ProductPicker.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Molitor\Product\Forms\Components;
+
+use Filament\Forms\Components\Field;
+
+class ProductPicker extends Field
+{
+    protected string $view = 'product::forms.components.product-picker';
+}
