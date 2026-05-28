e66323cfdeb31cb937fc5fb58165311f1de792b2
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-05-28 11:56:40 +0200

    test

---
+++ tests/Feature/PackageSmokeTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Molitor\Product\Tests\Feature;
+
+use Molitor\Product\Providers\ProductServiceProvider;
+use Tests\TestCase;
+
+class PackageSmokeTest extends TestCase
+{
+    public function test_service_provider_is_loaded(): void
+    {
+        $this->assertTrue(class_exists(ProductServiceProvider::class));
+        $this->assertTrue($this->app->providerIsLoaded(ProductServiceProvider::class));
+    }
+}
+
